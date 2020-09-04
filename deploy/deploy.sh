3767a815e1bfbf819cd26308b01652e2eeae8bb7
Costin Bleotu <costi.bleotu@gmail.com>
2020-09-04 11:18:54 +0300

    Added echos

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,10 @@
 cd /root/paul-web/app
+echo "Pull git repository"
 git pull
+echo "Install new dependencies"
 yarn
+echo "Build"
 yarn build
+echo "Copy dist to final destination"
 cp -R dist/* /var/www/paul-web
 echo "Done!"
